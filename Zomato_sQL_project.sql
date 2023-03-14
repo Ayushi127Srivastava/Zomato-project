acc3e017532536ea0e15b1343be1d015eb564928
Ayushi127Srivastava <101967474+Ayushi127Srivastava@users.noreply.github.com>
2023-03-14 12:02:36 +0530

    Update Zomato_sQL_project.sql

--- Zomato_sQL_project.sql
+++ Zomato_sQL_project.sql
@@ -1,3 +1,5 @@
+#Creating table and inserting date into it 
+
 drop table if exists goldusers_signup;
 CREATE TABLE goldusers_signup(userid integer,gold_signup_date date); 
 
@@ -45,7 +47,7 @@ INSERT INTO product(product_id,product_name,price)
 (3,'p3',330);
 
 
-           What is the total amount each customer spent on Zomato?
+Question1-  What is the total amount each customer spent on Zomato?
 
 
 SELECT sales.userid, product.product_id ,sum(product.price) as total_amount_spent
@@ -58,7 +60,7 @@ FROM sales inner join  product
 on sales.product_id=product.product_id
 GROUP BY sales.userid 
 
-      how many days has each customer visited zomato?
+Question2-  how many days has each customer visited zomato?
 
 SELECT * FROM goldusers_signup
 SELECT * FROM product
@@ -69,13 +71,13 @@ SELECT sales.userid ,count(distinct(sales.created_date)) as number_of_days
 FROM sales
 GROUP BY sales.userid
 
-       What is the first product purchased by each customer ?
+ Question3- What is the first product purchased by each customer ?
 
 SELECT *
 FROM(SELECT *,RANK() over(partition by userid order by created_date ) rnk FROM sales) a
 WHERE rnk=1
 
-     What is the most purchased items on the menu and how many times it was purchased by all customer ?
+Question4- What is the most purchased items on the menu and how many times it was purchased by all customer ?
 
 
 SELECT distinct(product_id), count(product_id) as number_of_times_purchased
@@ -93,7 +95,7 @@ Group by product_id
 order by number_of_times_purchased desc
 
 
-    Which item was most popular for each customer ?
+ Question5- Which item was most popular for each customer ?
 
 SELECT userid , product_id,count(product_id) as fav_order
 FROM sales
@@ -105,7 +107,7 @@ SELECT * FROM
 (SELECT userid , product_id,count(product_id) as fav_order from sales group by userid,product_id) a) b 
 WHERE rnk=1
 
-   Which item was first purchased by the customer after they become the member?
+Question6=  Which item was first purchased by the customer after they become the member?
 
 
  SELECT * from
@@ -120,7 +122,7 @@ WHERE rnk=1
 
   
 
-  Which item was purchased just before the customer become gold member ?
+Question7-  Which item was purchased just before the customer become gold member ?
 
   SELECT * FROM
   (SELECT * ,rank() over(partition by userid order  by created_date desc)rnk from
@@ -132,7 +134,7 @@ WHERE rnk=1
   WHERE rnk=1
 
 
-      What is the total order and amount spent for each member before they become member ?
+ Question8- What is the total order and amount spent for each member before they become member ?
 
 SELECT * FROM goldusers_signup
 SELECT * FROM product
@@ -150,11 +152,8 @@ SELECT * FROM users
  GROUP BY a.userid
 
 
-    If buying each product generates point like - product1 p1 , 5Rs=1Zomato point and p2 , 10rs=5 zomato points and p3, 5rs=1zomato points
+Question9- If buying each product generates point like - product1 p1 , 5Rs=1Zomato point and p2 , 10rs=5 zomato points and p3, 5rs=1zomato points
       Calculate points collected by each customers and for which product most point have been given till now ?
-
-
-
 
 
 SELECT userid,sum(points_per_order) as points_earned_per_user
@@ -188,8 +187,8 @@ FROM
 GROUP BY d.product_id
 ORDER BY total_sum_points desc
 
-  In the first one year after a customer joins the gold program (including their join date ) irrespective of what the customer has purchased they 
-  earn 5 zomato points for every 10rs spent who earned more 1 or 3 and what was their points earning in their first year ?
+ Question10-  In the first one year after a customer joins the gold program (including their join date ) irrespective of what the customer has purchased they 
+                  earn 5 zomato points for every 10rs spent who earned more 1 or 3 and what was their points earning in their first year ?
 
 
 
@@ -207,14 +206,14 @@ ORDER BY total_sum_points desc
   Order by total_points_per_user  desc
 
   
-       Rank all the transaction fo the customer ?
+ Question11-  Rank all the transaction fo the customer ?
 
   SELECT * from sales
 
   SELECT * , rank ()over (partition by userid order by created_date )rnk 
   FROM sales
 
-      Rank all the transaction for each customer whenever they are a zomato gold member for every non gold member trasaction mark as null
+  Question12- Rank all the transaction for each customer whenever they are a zomato gold member for every non gold member trasaction mark as null
 
 SELECT * , case when rnk=0 then 'na' else rnk end as rnkk from
 (
